40c0dfb1daa490dd8e9725fb462488639f9ea157
Muthu rajkumar <muthurajkumar12@gmail.com>
2026-06-25 09:53:52 +0530

    PROJECT CREATED

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int product = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            product = product * i;
+        }
+
+        System.out.println(product);
+        sc.close();
+    }
+}
+
